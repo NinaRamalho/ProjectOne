bfe9c223895a68ad1d1be61e4b157a35fb8ea9fe
NinaRamalho <162907689+NinaRamalho@users.noreply.github.com>
2025-03-04 20:42:22 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,34 +1,11 @@
+import RoutesApp from "./routes";
 
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+function App(){
+    return(
+        <RoutesApp/>
+);
+}
+
+export default App;
 
 
-function App() {
-
-
-  const [tarefas, setTarefas ] = useState(['Pagar conta de luz', 'Estudar React']);
-  const [input, setInput] = useState('');     
-
-const handleAdd = useCallback(()=> {
-  setTarefas([...tarefas, input]);
-  setInput('');
-}, [input, tarefas]);
-
-const totalTarefas = useMemo(()=> tarefas.length, [tarefas]);
-
-  return(
-    <div>
-      <ul>
-        {tarefas.map(tarefa =>(                                                           
-          <li key={tarefa}>{tarefa}</li>
-        ))}
-      </ul>
-      <br/>
-      <strong>Você tem {totalTarefas} tarefas!</strong><br/>
-      <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
-      <button type='button' onClick={handleAdd}>Adicionar</button>
-    </div>
-
-      );
-    }
-     
-    export default App;
